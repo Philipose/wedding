6e0fc3406cdd77b2c53090acfe35163f8a01d875
Aaron <aaronphilipose@gmail.com>
2023-10-04 13:01:41 -0400

    Removing weeks for better mobile look

--- js/main.js
+++ js/main.js
@@ -254,10 +254,9 @@ jQuery(document).ready(function($) {
 
 	var siteCountDown = function() {
 
-		$('#date-countdown, #date-countdown2').countdown('2024/05/04', function(event) {
+		$('#date-countdown').countdown('2024/05/04', function(event) {
 		  var $this = $(this).html(event.strftime(''
-		    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
-		    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
+		    + '<span class="countdown-block"><span class="label">%D</span> days </span>'
 		    + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
 		    + '<span class="countdown-block"><span class="label">%M</span> min </span>'
 		    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
