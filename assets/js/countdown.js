ed1cf24a637f0519503385888a36993f35354529
Aaron <aaronphilipose@gmail.com>
2023-11-23 14:29:46 -0500

    Cleaning up smells

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -2,9 +2,7 @@ function countdown_spanner(time, unit){
     return '<span class="label">' + time + ' ' + unit + '</span> ';
 };
 function new_countdown(target_date) {
-    var target_time = new Date(target_date).getTime();
-    var current_time = new Date().getTime();
-    var time_left = new Date(target_time - current_time);
+    const time_left = new Date(target_date).getTime() - new Date().getTime();
 
     return (countdown_spanner(Math.floor(time_left / (1000 * 60 * 60 * 24)), 'days')
             + countdown_spanner(Math.floor((time_left % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)), 'hr')
@@ -13,7 +11,7 @@ function new_countdown(target_date) {
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-    var siteCountDown = function() {
+     const siteCountDown = function() {
         document.getElementById('date-countdown').innerHTML = new_countdown('2024/05/04');
     };
     setInterval(siteCountDown, 1000);
