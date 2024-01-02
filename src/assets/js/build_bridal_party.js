5ca1b8f41512a71941fea7d807e9b037d0aea7cf
Aaron <aaronphilipose@gmail.com>
2024-01-02 09:01:37 -0500

    retool bridal party builder to have title

--- src/assets/js/build_bridal_party.js
+++ src/assets/js/build_bridal_party.js
@@ -1,56 +1,50 @@
 function build_bridal_party() {
-    const bridal_party_data = {
-        'filter-bridesmaid': [
-          ['Kesia', 'George'],
-          ['Rebecca', 'Chacko'],
-          ['Christina', 'George'],
-          ['Celia', 'George'],
-          ['Supriya', 'Paidemarry'],
-          ['Minu', 'Pitchiah'],
-          ['Reena', 'Mathew'],
-          ['Shalini', 'Tummala'],
-          ['Jocelyn', 'Augustine'],
-          ['Erika', 'Malana']
-        ],
-        'filter-groomsman': [
-          ['Michael', 'Joshua'],
-          ['Stephen', 'Chacko'],
-          ['Nebu', 'John'],
-          ['Haris', 'Ahmad'],
-          ['George', 'Teodorescu'],
-          ['Jeremy', 'Abraham']
-        ],
-    }
-    const honoured_attendants = {
-      'Kesia': "Maid of Honour",
-      'Rebecca': "Matron of Honour",
-      'Michael': "Best Man",
-      'Stephen': "Best Man",
-    }
-    for (const [role, person] of Object.entries(bridal_party_data)) {
-        for (const name of person) {
-          first_name = name[0];
-          last_name = name[1];
-          document.getElementById('BridalPartyTable').innerHTML += `          <div id=${first_name} class="col-6 col-lg-3 col-md-6 portfolio-item ${role}">
-          <div class="portfolio-wrap">
-            <img src="assets/img/portfolio/${first_name}.webp" loading="lazy" width="315" height= "315" class="img-fluid" alt="">
-            <div class="portfolio-links">
-              <a id=${first_name}_label>${first_name} ${last_name}</a>
-            </div>
-          </div>
+    const bridal_party = [
+      {'FirstName': 'Kesia', 'LastName': 'George', 'Roles': ['Bridesmaid', 'Maid of Honour']},
+      {'FirstName': 'Rebecca', 'LastName': 'Chacko', 'Roles': ['Bridesmaid', 'Matron of Honour']},
+      {'FirstName': 'Christina', 'LastName': 'George', 'Roles': ['Bridesmaid']},
+      {'FirstName': 'Celia', 'LastName': 'George', 'Roles': ['Bridesmaid']},
+      {'FirstName': 'Supriya', 'LastName': 'Paidemarry', 'Roles': ['Bridesmaid']},
+      {'FirstName': 'Minu', 'LastName': 'Pitchiah', 'Roles': ['Bridesmaid']},
+      {'FirstName': 'Reena', 'LastName': 'Mathew', 'Roles': ['Bridesmaid']},
+      {'FirstName': 'Shalini', 'LastName': 'Tummala', 'Roles': ['Bridesmaid']},
+      {'FirstName': 'Jocelyn', 'LastName': 'Augustine', 'Roles': ['Bridesmaid']},
+      {'FirstName': 'Erika', 'LastName': 'Malana', 'Roles': ['Bridesmaid']},
+      {'FirstName': 'Michael', 'LastName': 'Joshua', 'Roles': ['Groomsman', 'Best Man']},
+      {'FirstName': 'Stephen', 'LastName': 'Chacko', 'Roles': ['Groomsman', 'Best Man']},
+      {'FirstName': 'Nebu', 'LastName': 'John', 'Roles': ['Groomsman']},
+      {'FirstName': 'Haris', 'LastName': 'Ahmad', 'Roles': ['Groomsman']},
+      {'FirstName': 'George', 'LastName': 'Teodorescu', 'Roles': ['Groomsman']},
+      {'FirstName': 'Jeremy', 'LastName': 'Abraham', 'Roles': ['Groomsman']},
+    ]
+
+    bridal_party.forEach(function(person){
+      document.getElementById('BridalPartyTable').innerHTML +=
+`          <div id=${person.FirstName} class="col-6 col-lg-3 col-md-6 portfolio-item">
+      <div class="portfolio-wrap">
+        <img src="assets/img/portfolio/${person.FirstName}.webp" loading="lazy" width="315" height= "315" class="img-fluid" alt="">
+        <div class="portfolio-links">
+          <a id=${person.FirstName + '_label'}>${person.FirstName} ${person.LastName}</a>
         </div>
-  `
-        }
-    }
+      </div>
+`
+      roles = Array.from(person.Roles);
+      roles.forEach(function(role){
+        add_role(person.FirstName, person.LastName, role);
+      });
+    });
+  };
 
-    for (const [person, title] of Object.entries(honoured_attendants)){
-        add_honour(person, title);
+function add_role(first_name, last_name, role){
+    role_map = {
+      'Maid of Honour': 'filter-honoured',
+      'Matron of Honour': 'filter-honoured',
+      'Best Man': 'filter-honoured',
+      'Groomsman': 'filter-groomsman',
+      'Bridesmaid': 'filter-bridesmaid',
     }
-}
-
-function add_honour(person, title){
-    document.getElementById(person).classList.add('filter-honoured');
-    document.getElementById(person.toString() + '_label').innerHTML += "<br/>" + title.toString();
+    document.getElementById(first_name).classList.add(role_map[role]);
+    document.getElementById(first_name.toString() + '_label').innerHTML = `${first_name} ${last_name}<br/>` + role.toString();
 }
 
 function clear_bridal_party(){
