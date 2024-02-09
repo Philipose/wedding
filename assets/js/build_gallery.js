1cdce27167e3e0eb0d0071440d3fbb28f8564938
Aaron <aaronphilipose@gmail.com>
2024-02-08 19:58:25 -0500

    gallery update

--- assets/js/build_gallery.js
+++ assets/js/build_gallery.js
@@ -5,11 +5,10 @@ function populateGallery() {
         document.getElementById('Gallery-Wheel').innerHTML +=`
         <div>
         <a class="spotlight" href="assets/img/gallery/full/${i}.webp">
-        <img src="assets/img/gallery/thumb/${i}.webp">
+        <img src="assets/img/gallery/thumb/${i}.webp" alt="Gallery Image ${i}" />
         </a>
         </div>`;
     }
-    // document.getElementById('morePicturesDiv').innerHTML = '';
     $('.gallery-wheel').slick({
         dots: false,
         infinite: false,
