94704836f42bc3c74423d5e72dc77fb70bbb51cf
Aaron <aaronphilipose@gmail.com>
2024-02-08 20:24:58 -0500

    update gallery

--- src/assets/js/build_gallery.js
+++ src/assets/js/build_gallery.js
@@ -4,17 +4,18 @@ function populateGallery() {
     for (let i = 0; i <= numberOfImages; i++) {
         document.getElementById('Gallery-Wheel').innerHTML +=`
         <div>
-        <a class="spotlight" href="assets/img/gallery/full/${i}.webp" aria-label="Fullscreen Gallery Image ${i}" aria-hidden="true">
-        <img src="assets/img/gallery/thumb/${i}.webp" alt="Gallery Image ${i}"/>
+        <a class="spotlight" href="assets/img/gallery/full/${i}.webp" aria-label="Fullscreen Gallery Image ${i+1}">
+        <img src="assets/img/gallery/thumb/${i}.webp" alt="Image ${i+1}"/>
         </a>
         </div>`;
     }
     $('.gallery-wheel').slick({
         dots: false,
         infinite: false,
-        speed: 300,
         slidesToShow: 1,
         centerMode: false,
-        variableWidth: true
+        variableWidth: true,
+        lazyLoad: 'ondemand',
+        mobileFirst: true
       });    
 }
