83796113fb458c6dc5de566e13babb3371fd475b
Aaron <aaronphilipose@gmail.com>
2024-02-19 21:19:19 -0500

    Readding icons for clarity

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,6 +93,8 @@
    */
   on('click', '.mobile-nav-toggle', function(e) {
     select('body').classList.toggle('mobile-nav-active')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
   })
 
   /**
