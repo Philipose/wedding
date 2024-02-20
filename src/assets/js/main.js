73f2ae74dbf30c8120d3c49a07946a147b90e6d3
Aaron <aaronphilipose@gmail.com>
2024-02-19 21:11:24 -0500

    Menu remove the X for now.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -93,8 +93,6 @@
    */
   on('click', '.mobile-nav-toggle', function(e) {
     select('body').classList.toggle('mobile-nav-active')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
   })
 
   /**
